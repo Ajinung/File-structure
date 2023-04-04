d7387a5cd43aec58fd81b51420ebaef8752120a5
Ajinung <isaacetor7@gmail.com>
2023-04-04 20:04:51 +0100

    deletd seventh commit

--- src/pages/Home.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Footer, Header } from "../components";
-import { Outlet } from "react-router-dom";
-
-const Home = () => {
-  return <div>Home</div>;
-};
-
-export default Home;
